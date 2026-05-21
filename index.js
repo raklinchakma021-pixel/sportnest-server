b9a45331f570db1b07f4eb3d631a0f05d0849f44
Raklin Chakma <raklinchakma021@gmail.com>
2026-05-21 10:11:41 +0600

    for the jwt integration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express")
 const dontenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dontenv.config();
 
 const uri =  process.env.MONGODB_URI;
@@ -24,6 +24,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -40,7 +64,7 @@ app.get("/facility", async (req, res) => {
       res.json(result);
     });
 
-   app.get("/facility/:id", async (req, res) => {
+   app.get("/facility/:id",  verifyToken, async (req, res) => {
       const { id } = req.params;
 
       const result = await facilityCollection.findOne({
@@ -84,7 +108,7 @@ app.post("/facility", async (req, res) => {
     });
 
 
-  app.post("/booking", async (req, res) => {
+  app.post("/booking",verifyToken, async (req, res) => {
       const bookingData = req.body;
       const result = await bookingCollection.insertOne(bookingData);
 
@@ -98,7 +122,7 @@ app.post("/facility", async (req, res) => {
 
       res.json(result);
     });
- app.delete("/booking/:bookingId", async (req, res) => {
+ app.delete("/booking/:bookingId",verifyToken, async (req, res) => {
       const { bookingId } = req.params;
       const result = await bookingCollection.deleteOne({
         _id: new ObjectId(bookingId),
